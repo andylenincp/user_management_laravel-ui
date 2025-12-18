6f49da995db2ef3c370c54f4c110c30540022c84
Andy Castillo Palma <andy.castillopalma@outlook.com>
2025-12-18 17:02:43 -0500

    User fields in migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', length: 50);
+            $table->string('last_name', length: 50);
+            $table->string('email', length: 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('type', ['administrator', 'editor', 'viewer'])->default('viewer');
+            $table->string('description', length: 200);
             $table->rememberToken();
             $table->timestamps();
         });
